1a1b276b23dd073e0fc02ef0ac9ecc3a4a9ab749
Sohottoday <58559786+Sohottoday@users.noreply.github.com>
2020-09-23 12:50:17 +0900

    20200923

--- titanic/controller.py
+++ titanic/controller.py
@@ -5,18 +5,24 @@ import numpy as np
 from titanic.entity import Entity
 from titanic.service import Service
 
+"""
+PassengerId  고객ID,
+Survived 생존여부,
+Pclass 승선권 1 = 1등석, 2 = 2등석, 3 = 3등석,
+Name,
+Sex,
+Age,
+SibSp 동반한 형제, 자매, 배우자,
+Parch 동반한 부모, 자식,
+Fare 요금,
+Embarked 승선한 항구명 C = 쉐브루, Q = 퀸즈타운, S = 사우스햄튼
+
+"""
+
 class Controller:
     def __init__(self):
         self.entity = Entity()
         self.service = Service()
-
-    
-    def preprocessing(self, train, test): # -> object
-        service = self.service
-        this = self.entity
-        this.train = service.new_model(train)       # payload
-        this.test = service.new_model(test)         # payload
-        return this
 
 
     def modeling(self, train, test): # -> object
@@ -25,6 +31,20 @@ class Controller:
         print(f'훈련 컬럼 : {this.train.columns}')
         this.label = service.create_label(this)
         this.train = service.create_train(this)
+
+        return this
+
+
+    def preprocessing(self, train, test): # -> object
+        service = self.service
+        this = self.entity
+        this.train = service.new_model(train)       # payload
+        this.test = service.new_model(test)         # payload
+        this.id = this.test['PassengerId']          # machine에게는 이것이 question이 된다.
+        print(f'드롭 전 변수 : {this.train.columns}')
+        this = service.drop_feature(this, 'Cabin')
+        this = service.drop_feature(this, 'Ticket')
+        print(f'드롭 후 변수 : {this.train.columns}')
         return this
 
 
@@ -32,7 +52,7 @@ class Controller:
         pass
 
 
-    def submit(self):
+    def submit(self):           # machine이 된다. 이 단계에서는 캐글에게 내 머신을 보내서 평가받게 하는 것.
         pass
 
 if __name__ == '__main__':
--- titanic/service.py
+++ titanic/service.py
@@ -4,23 +4,22 @@ sys.path.insert(0, '/Users/user/SbaProjects')
 import pandas as pd
 import numpy as np
 
+
 """
-PassengerId  고객ID,
-Survived 생존여부,
+### PassengerId  고객ID,           @ 문제
+### Survived 생존여부,              @ 머신러닝 모델이 맞춰야 할 답
 Pclass 승선권 1 = 1등석, 2 = 2등석, 3 = 3등석,
 Name,
 Sex,
 Age,
 SibSp 동반한 형제, 자매, 배우자,
 Parch 동반한 부모, 자식,
-Ticket 티켓번호,
+### Ticket 티켓번호,                @ 쓰레기 값
 Fare 요금,
-Cabin 객실번호,
+### Cabin 객실번호,                 @ 쓰레기 값
 Embarked 승선한 항구명 C = 쉐브루, Q = 퀸즈타운, S = 사우스햄튼
 
 """
-
-
 
 class Service:
     def __init__(self):
@@ -50,6 +49,86 @@ class Service:
         this.test = this.test.drop([feature], axis=1)       # p.140에 보면 훈련, 테스트 세트로 나눈다.
         return this
 
+
+
+    @staticmethod
+    def pclass_ordinal(this) -> object: 
+        return this
+
+    @staticmethod
+    def name_nominal(this) -> object:
+        return this
+
+    @staticmethod
+    def sex_nominal(this) -> object:        # male = 0, female = 1
+        combine = [this.train, this.test]       # 반복된 코드를 피하기 위해 사용, train과 test가 묶인다.
+        sex_mapping = {'mail':0, 'female':1}
+        for dataset in combine:
+            dataset['Sex'] = dataset['Sex'].map(sex_mapping)
+
+        this.train = this.train     # overriding
+        this.test = this.test
+
+        # this.train = combine[0]
+        # this.test = combine[1]
+
+        # this.train['Sex'] = this.train['Sex'].map({'male':0, 'female':1})
+        # this.test['Sex'] = this.test['Sex'].map({'male':0, 'female':1})
+        return this
+
+    @staticmethod
+    def age_ordinal(this) -> object:
+        train = this.train
+        test = this.test
+        train['Age'] = train['Age'].fillna(-0.5)            # age는 평균으로 넣기도 애매하고, 다수결로 넣기도 근거가 없다. 특히 age는 생존률 판단에서 가중치(weight)가 높으므로 디테일한 접근이 필요
+        test['Age'] = test['Age'].fillna(-0.5)              # 나이를 모르는 승객은 모르는 상태로 처리해야 값의 왜곡을 줄일 수 있기 때문에 -0.5라는 경계값(어느쪽에도 속하지 않는 값)으로 처리
+        bins = [-1, 0, 5, 12, 18, 24, 35, 60, np.inf]       # 안의 숫자는 양 숫자 사이의 범위를 의미
+        labels = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']      # [] 에 있으므로 변수명으로 판단,
+        train['AgeGroup'] = pd.cut(train['Age'], bins, labels=labels)
+        test['AgeGroup'] = pd.cut(test['Age'], bins, labels=labels)
+        age_title_mapping = {
+            0: 'Unknown',
+            1: 'Baby',
+            2: 'Child',
+            3: 'Teenager',
+            4: 'Student',
+            5: 'Young Adult',
+            6: 'Adult',
+            7: 'Senior'
+        }   # []에서 {}으로 처리하면 labels를 값으로 처리하겠다는 의미
+        return this
+
+    @staticmethod
+    def sibsp_numeric(this) -> object:
+        return this
+
+    @staticmethod
+    def parch_numeric(this) -> object:
+        return this
+
+    @staticmethod
+    def fare_ordinal(this) -> object:
+        this.train['FareBand'] = pd.qcut(this['Fare'], 4, labels={1, 2, 3, 4})          # qcut 은 분할 해준다는 의미(분할하려는것, 분할하려는 등분(여기서는 4이므로 4등분), 부여하려는 값)
+        this.test['FareBand'] = pd.qcut(this['Fare'], 4, labels={1, 2, 3, 4})
+        return this
+
+    @staticmethod
+    def fareBand_nominal(this) -> object:           # 요금이 다양하니 클러스터링을 하기위한 준비
+        this.train = this.train.fillna({'FareBand' : 1})        # FareBand는 존재하지 않는 변수지만 추가시킴
+        this.test = this.test.fillna({'FareBand' : 1})
+        return this
+
+
+
+    @staticmethod
+    def embarked_nominal(this) -> object:
+        this.train = this.train.fillna({'Embarked': 'S'})
+        this.test = this.test.fillna({'Embarked': 'S'})
+        # 많은 머신러닝 라이브러리는 클래스 레이블이 '정수'로 인코딩 되었다고 기대함
+        # 교과서 146 문자 blue = 0, green = 1, red = 2 로 치환
+        this.train['Embarked'] = this.train['Embarked'].map({'S':1, 'C':2, 'Q':3})        # ordinal 아님
+        this.test['Embarked'] = this.test['Embarked'].map({'S':1, 'C':2, 'Q':3})
+        return this
 
 
     
