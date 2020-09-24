c608fd38d2cf5589408c22e77bbb844726a81629
Sohottoday <58559786+Sohottoday@users.noreply.github.com>
2020-09-24 18:58:16 +0900

    price_predicion update

--- price_prediction/cabbage.py
+++ price_prediction/cabbage.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.insert(0, '/Users/user/SbaProjects')
-from utill.file_handler import FileReader
+
+from util.file_handler import FileReader
 
 import pandas as pd
 import numpy as np
@@ -21,24 +22,31 @@ class Model:
         xy = np.array(payload, dtype=np.float32)
         x_data = xy[:, 1:-1]        # feature
         y_data = xy[:, [-1]]        # price
-        X = tf.placeholder(tf.float32, shape=[None, 4])
-        Y = tf.placeholder(tf.float32, shape=[None, 2])
-        W = tf.Variable(tf.random_normal([4, 1]), name='weight')
-        b = tf.Variable(tf.random_normal([1]), name='bias')
+        X = tf.compat.v1.placeholder(tf.float32, shape=[None, 4])
+        Y = tf.compat.v1.placeholder(tf.float32, shape=[None, 1])
+        W = tf.Variable(tf.random.normal([4, 1]), name='weight')
+        b = tf.Variable(tf.random.normal([1]), name='bias')
         hyposthesis = tf.matmul(X, W) + b
         cost = tf.reduce_mean(tf.square(hyposthesis - Y))
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.000005)
-        train = optimizer.minmize(cost)
-        sess = tf.Session()
-        sess.run(tf.global_variables_initializer())
+        optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=0.000005)
+        train = optimizer.minimize(cost)
+        sess = tf.compat.v1.Session()
+        sess.run(tf.compat.v1.global_variables_initializer())
         for step in range(100000):
-            cost_, hypo_, = sess.run([cost, hyposthesis, train], feed_dict={X:x_data, Y:y_data})
+            cost_, hypo_, _ = sess.run([cost, hyposthesis, train], feed_dict={X:x_data, Y:y_data})
             if step % 500 == 0:
                 print(f'# {step} 손실비용 : {cost_}')
                 print(f'- 배추가격 : {hypo_[0]}')
 
         saver = tf.train.Saver()
         print('저장 완료')
+
+
+if __name__ == "__main__":
+    m = Model()
+    dframe = m.new_model('price_data.csv')
+    print(dframe.head())
+    m.create_tf(dframe)
 
 
 
